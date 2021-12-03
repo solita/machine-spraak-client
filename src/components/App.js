5207789165b94f004c953b1c3455ca6fa7c5e4f5
Ilari Junnonen <ilari.junnonen@solita.fi>
2021-12-03 11:18:48 +0200

    Add debug prints

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
+import Spinner from "react-bootstrap/Spinner";
 import "./App.css";
 import Amplify, { API } from "aws-amplify";
 
@@ -19,18 +20,34 @@ Amplify.configure({
 
 const App = () => {
   const [file, setFile] = useState(null);
+  const [response, setResponse] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const fileInputRef = useRef();
 
   const handleSelectFile = (event) => {
     setFile(event.target.files[0]);
+    setResponse("");
+    setError("");
+  };
+
+  const handleReset = () => {
+    fileInputRef.current.value = "";
+    setFile(null);
   };
 
   const handleUpload = async () => {
     try {
+      setLoading(true);
       const response = await API.get("machinespraak_api", "/audio_analysis");
-      console.log(response);
+      setResponse(response);
     } catch (error) {
       console.log(error);
+      setError(error.message);
     }
+    setLoading(false);
+    handleReset();
   };
 
   return (
@@ -45,17 +62,23 @@ const App = () => {
               type="file"
               size="lg"
               onChange={(event) => handleSelectFile(event)}
+              ref={fileInputRef}
             />
           </Form.Group>
         </Col>
       </Row>
-      <Row className="justify-content-center mt-0 mb-0">
+      <Row className="justify-content-center mt-2 mb-0">
         <Col md="10" xs="10">
           <div>
             <pre>
-              <button onClick={handleUpload}>Upload</button>{" "}{file && file.size + " bytes"}{file && file.type && ", " + file.type}
+              {file && <button onClick={handleUpload}>Upload</button>}{" "}
+              {file && file.name + " " + file.size + " bytes"}
+              {file && file.type && ", " + file.type}
+              {response}
+              {error}
             </pre>
           </div>
+          <div className="mt-1">{loading && <Spinner animation="border" role="status" />}</div>
         </Col>
       </Row>
     </Container>
