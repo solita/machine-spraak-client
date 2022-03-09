44cb53bc7a29f9deea0be511e43506610c5c84fc
Chathura Yapa <chathura.yapa@solita.fi>
2022-03-09 11:56:07 +0200

    Added AudioAnalysisService with PostFile HTTP request

---
+++ src/services/AudioAnalysisService.ts
@@ -0,0 +1,40 @@
+import Amplify, { API } from "aws-amplify";
+import {UNEXPECTED_SERVER_ERROR} from "./common";
+
+const MACHINE_SPRAAK_API_NAME = "machinespraak_api";
+
+const RESOURCES = {
+  AudioAnalysisResource:
+    {
+      name: MACHINE_SPRAAK_API_NAME ,
+      endpoint: process.env.REACT_APP_API_URL,
+    }
+};
+
+Amplify.configure({
+    API: {
+      endpoints: [
+        RESOURCES.AudioAnalysisResource,
+      ],
+    },
+});
+
+//TODO: Change the type "any" to appropriate type
+async function postFile(file:any):Promise<string>{
+  try{
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("file_name", file.name);
+    const content = {
+      body: formData,
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    };
+    return await API.post(RESOURCES.AudioAnalysisResource.name, "/audio_analysis",content);
+  }
+  catch{
+      return UNEXPECTED_SERVER_ERROR;
+  }
+};
+export default {postFile};
