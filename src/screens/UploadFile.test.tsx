158aca43d968db2cb675132acc374821b06b845c
Chathura Yapa <chathura.yapa@solita.fi>
2022-03-16 09:59:26 +0200

    Unit tests added

---
+++ src/screens/UploadFile.test.tsx
@@ -0,0 +1,15 @@
+import { render,screen } from "@testing-library/react";
+import React, { ReactElement } from "react";
+import UploadFile from "./UploadFile";
+
+describe("UploadFile", ()=>{
+    let testComponent:ReactElement;
+    beforeEach(()=>{
+        testComponent = <UploadFile/>
+    });
+
+    it("Rendering the component",()=>{
+        render(testComponent);
+        expect(screen.getByRole("heading",{name:"Upload a file"}));
+    });
+})
